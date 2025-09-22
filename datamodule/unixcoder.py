3ebfe294e071180aed471c932a2c98c920b5001e
kamikazizen <kamikazizen@gmail.com>
2025-09-22 17:05:23 +0300

    update unixcoder dataloader

--- datamodule/unixcoder.py
+++ datamodule/unixcoder.py
@@ -9,9 +9,6 @@ import math
 import random
 from preprocess import AST
 
-from .utils import pack_fim_inputs
-from .const import PARAPHRASE_CUES
-
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
@@ -196,13 +193,12 @@ class CodeCfcDataset(Dataset):
 
         else:
 
-            # preliminary truncating to save memory and avoid warnings
-            left_context_ids = self.code_tokenizer(self.data[ind]['content']['prompt'], return_tensors='pt', truncation=True, max_length=self.max_seq_length).input_ids[0]
-            right_context_ids = self.code_tokenizer(self.data[ind]['content']['right_context'], return_tensors='pt', truncation=True, max_length=self.max_seq_length).input_ids[0]
-            target_ids = self.code_tokenizer(self.data[ind]['content']['groundtruth'], return_tensors='pt', truncation=True, max_length=self.max_seq_length).input_ids[0]
+            left_context_ids = self.code_tokenizer(self.data[ind]['content']['prompt'], return_tensors='pt').input_ids[0]
+            right_context_ids = self.code_tokenizer(self.data[ind]['content']['right_context'], return_tensors='pt').input_ids[0]
+            groundtruth = "\n".join(self.data[ind]['content']['groundtruth'].split("\n")[:self.num_truth_lines])
+            target_ids = self.code_tokenizer(groundtruth, return_tensors='pt', truncation=True, max_length=self.max_seq_length).input_ids[0]
 
-            tgt_len = len(target_ids)
-            lr_budget = self.max_seq_length - tgt_len - self.num_structure_tokens - 3  # 3 tokens for FIM
+            lr_budget = self.max_seq_length - 50 - self.num_structure_tokens - 3  # 3 tokens for FIM, 50 for line completion
             rc_budget = int(lr_budget / (self.lc_rc_ratio + 1))
             lc_budget = int(rc_budget * self.lc_rc_ratio)
 
@@ -226,20 +222,20 @@ class CodeCfcDataset(Dataset):
                 left_context_ids,
                 fim_suffix_id,
                 right_context_ids,
-                self.code_tokenizer('# Here are some relevant code fragments from other files of the repo:', return_tensors='pt').input_ids[0],
+                self.code_tokenizer('\n# Here are some relevant code fragments from other files of the repo:', return_tensors='pt').input_ids[0],
                 torch.tensor([self.structure_token_id] * self.num_structure_tokens),
                 fim_middle_id,
                 target_ids]).to(torch.long)
 
             # for KL-div training
             all_cfc = '\n'.join(self.data[ind]['content']['crossfile_array'][:self.num_structure_tokens])
+            all_cfc = '\n# Here are some relevant code fragments from other files of the repo:\n' + all_cfc
             all_cfc_ids = self.code_tokenizer(all_cfc, return_tensors='pt', truncation=True, max_length=self.max_seq_length).input_ids[0]
             teacher_input_ids = torch.cat([
                 fim_prefix_id,
                 left_context_ids,
                 fim_suffix_id,
                 right_context_ids,
-                self.code_tokenizer('# Here are some relevant code fragments from other files of the repo:', return_tensors='pt').input_ids[0],
                 all_cfc_ids,
                 fim_middle_id,
                 target_ids]).to(torch.long)
@@ -313,18 +309,25 @@ class AstCfcDataset(Dataset):
                  code_tokenizer,
                  structure_token_id,
                  fim_tokens_ids,
+                 num_truth_lines=10,
                  num_structure_tokens=None,
                  max_seq_length=2048,
                  max_structure_length=512,
                  lc_rc_ratio=2.0):
         super(AstCfcDataset, self).__init__()
         self.data = data
+        print('Dataset samples before removal: ', len(data))
+        remove_indices = [4689, 4690, 10037, 10998, 13381, 14865, 15364, 17490, 20118, 32910, 39973, 41641, 46718, 58023, 58643, 58856, 64421, 68036, 68990, 72104, 72105, 72690, 72691, 73997, 75598, 81033, 88847, 90688, 93281, 95307, 95500, 95606, 107740, 107742, 114358, 115832, 120194, 134935, 136458]
+        keep_indices = [i for i in range(len(data)) if i not in remove_indices]
+        self.data = data.select(keep_indices)
+        print('Dataset samples after removal: ', len(self.data))
         self.max_seq_length = max_seq_length
         self.code_tokenizer = code_tokenizer
         self.ast_tokenizer = ast_tokenizer
         self.structure_token_id = structure_token_id
         self.max_structure_length = max_structure_length
         self.lc_rc_ratio = lc_rc_ratio
+        self.num_truth_lines = num_truth_lines
         self.num_structure_tokens = num_structure_tokens
         self.fim_tokens_ids = fim_tokens_ids
         self.training_stage = training_stage
@@ -333,6 +336,9 @@ class AstCfcDataset(Dataset):
         return len(self.data)
 
     def __getitem__(self, ind):
+
+        fim_prefix_id, fim_suffix_id, fim_middle_id = self.fim_tokens_ids.reshape(3, 1)
+
         if self.training_stage == 0:
             # instead of unraveling this into 10x bigger dataset, just choose a random and increase number of epochs
             chunk = random.choice(self.data[ind]['content']['crossfile_array'])
@@ -353,12 +359,10 @@ class AstCfcDataset(Dataset):
 
             left_context_ids = self.code_tokenizer(self.data[ind]['content']['prompt'], return_tensors='pt').input_ids[0]
             right_context_ids = self.code_tokenizer(self.data[ind]['content']['right_context'], return_tensors='pt').input_ids[0]
-            target_ids = self.code_tokenizer(self.data[ind]['content']['groundtruth'], return_tensors='pt').input_ids[0]
+            groundtruth = "\n".join(self.data[ind]['content']['groundtruth'].split("\n")[:self.num_truth_lines])
+            target_ids = self.code_tokenizer(groundtruth, return_tensors='pt', truncation=True, max_length=self.max_seq_length).input_ids[0]
 
-            tgt_len = len(target_ids)
-            if not self.num_structure_tokens:
-                self.num_structure_tokens = len(self.data[ind]['content']['crossfile_array'])
-            lr_budget = self.max_seq_length - tgt_len - self.num_structure_tokens - 3  # 3 tokens for FIM
+            lr_budget = self.max_seq_length - 50 - self.num_structure_tokens - 3  # 3 tokens for FIM, 50 for line completion
             rc_budget = int(lr_budget / (self.lc_rc_ratio + 1))
             lc_budget = int(rc_budget * self.lc_rc_ratio)
 
@@ -376,10 +380,30 @@ class AstCfcDataset(Dataset):
                 structure_ids.append(F.pad(torch.tensor(chunk_ids), (0, self.max_structure_length-len(chunk_ids)), value=self.ast_tokenizer.pad_token_id))
             structure_ids = torch.hstack(structure_ids)
 
-            input_ids = pack_fim_inputs(
-                self.fim_tokens_ids, left_context_ids, right_context_ids, target_ids, self.structure_token_id, self.num_structure_tokens)
+            input_ids = torch.cat([
+                fim_prefix_id,
+                left_context_ids,
+                fim_suffix_id,
+                right_context_ids,
+                self.code_tokenizer('\n# Here are some relevant code fragments from other files of the repo:', return_tensors='pt').input_ids[0],
+                torch.tensor([self.structure_token_id] * self.num_structure_tokens),
+                fim_middle_id,
+                target_ids]).to(torch.long)
 
-            item = {"input_ids": input_ids, 'structure_ids': structure_ids, 'num_structure_tokens': self.num_structure_tokens}
+            # for KL-div training
+            all_cfc = '\n'.join(self.data[ind]['content']['crossfile_array'][:self.num_structure_tokens])
+            all_cfc = '\n# Here are some relevant code fragments from other files of the repo:\n' + all_cfc
+            all_cfc_ids = self.code_tokenizer(all_cfc, return_tensors='pt', truncation=True, max_length=self.max_seq_length).input_ids[0]
+            teacher_input_ids = torch.cat([
+                fim_prefix_id,
+                left_context_ids,
+                fim_suffix_id,
+                right_context_ids,
+                all_cfc_ids,
+                fim_middle_id,
+                target_ids]).to(torch.long)
+
+            item = {"input_ids": input_ids, 'teacher_input_ids': teacher_input_ids, 'structure_ids': structure_ids, 'num_structure_tokens': self.num_structure_tokens}
 
         return item
 
