5d80c23f2ac1b3a98494c9d7f7f703d6b88eac00
KamikaziZen <kamikazizen@gmail.com>
2025-11-27 21:27:39 +0300

    java test

---
+++ test_java.py
@@ -0,0 +1,137 @@
+import os
+
+from transformers import (
+    AutoConfig,
+    AutoTokenizer,
+    RobertaConfig,
+    RobertaTokenizer,
+)
+
+import torch
+import lightning.pytorch as pl
+from lightning.pytorch.strategies import DDPStrategy
+from lightning.pytorch.callbacks import LearningRateMonitor, ModelCheckpoint
+from lightning.pytorch.loggers import CSVLogger
+from lightning.pytorch.utilities import rank_zero_only
+
+from huggingface_hub import login
+from dotenv import load_dotenv
+import logging
+
+from models import LlavaCodeConfig,  LlavaCodeForConditionalGeneration
+from pl_args import add_model_args, add_pl_args, add_program_args
+from datamodule import LlavaCodeDataModule
+from datamodule.const import STRUCTURE_TOKEN, FIMMAP
+from datamodule.utils import get_fim_tokens
+from pl_logger import ClearMLLogger
+
+
+logging.basicConfig(
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S')
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+if __name__ == "__main__":
+
+    parser = add_program_args()
+    parser = add_model_args(parser)
+    parser = add_pl_args(parser)
+    args = parser.parse_args()
+    pl.seed_everything(args.seed, workers=True)
+
+    # User gives batch size over all GPUs, PL requires per GPU
+    args.train_batch_size = args.train_batch_size // (args.devices * args.num_nodes)
+    args.valid_batch_size = args.valid_batch_size // (args.devices * args.num_nodes)
+    logger.info(f'{args.train_batch_size=} {args.valid_batch_size=}')
+    # User gives validation check interval in terms of number of steps, PL requires in terms of batches
+    args.val_check_interval *= args.accumulate_grad_batches
+
+    print('args:', args)
+
+    code_tokenizer = AutoTokenizer.from_pretrained(args.text_model_id, use_fast=False)
+    code_tokenizer.add_tokens([STRUCTURE_TOKEN])
+    if code_tokenizer.pad_token_id is None:
+        code_tokenizer.pad_token_id = code_tokenizer.eos_token_id
+    structure_token_id = code_tokenizer.convert_tokens_to_ids(STRUCTURE_TOKEN)
+
+    structure_tokenizer = AutoTokenizer.from_pretrained(args.structure_model_id, use_fast=False)
+
+    structure_config = AutoConfig.from_pretrained(args.structure_model_id)
+    structure_config.model_id = args.structure_model_id
+    structure_config.pad_token_id = structure_tokenizer.pad_token_id
+
+    text_config = AutoConfig.from_pretrained(args.text_model_id)
+    text_config.model_id = args.text_model_id
+    assert len(code_tokenizer) <= text_config.vocab_size, 'The tokenizer length is larger than the embedding layer shape, resize the embeddings'
+    # text_config.vocab_size = text_config.vocab_size + 1  # for a new <CODE_STRUCTURE>
+    configuration = LlavaCodeConfig(structure_config, text_config,
+                                    pad_token_id=code_tokenizer.pad_token_id,
+                                    structure_token_id=structure_token_id,
+                                    projector=args.projector,
+                                    quantize=args.quantize)
+
+    if args.model_checkpoint is not None:
+        print(f"Loading checkpoint: {args.model_checkpoint}")
+        # model = LlavaCodeForConditionalGeneration.load_from_checkpoint(
+        #     args.model_checkpoint, config=configuration)
+        model = LlavaCodeForConditionalGeneration(
+            config=configuration, model_path=args.model_checkpoint) 
+    elif args.projector_checkpoint:
+        model = LlavaCodeForConditionalGeneration(configuration)
+        print(f'Loading projection weighs from : {args.projector_checkpoint}')
+        model.multi_modal_projector.load_state_dict(torch.load(args.projector_checkpoint))
+    else:
+        print('Initializing a pipeline')
+        model = LlavaCodeForConditionalGeneration(configuration)
+
+    data = LlavaCodeDataModule(
+        args.data_prefix,
+        args.train_datadir,
+        args.valid_datadir,
+        args.train_batch_size,
+        args.valid_batch_size,
+        fim_tokens=get_fim_tokens(args.text_model_id),
+        training_stage=args.training_stage,
+        num_workers=args.num_workers,
+        code_tokenizer=code_tokenizer,
+        structure_tokenizer=structure_tokenizer,
+        structure_token_id=structure_token_id,
+        num_structure_tokens=args.num_structure_tokens,
+    )
+    print('Training stage:', args.training_stage)
+    data.setup()
+    args.num_training_examples = len(data.train_dataloader())
+
+    logger.info('Initializing PL Trainer...')
+    custom_trainer_kwargs = {
+        "num_sanity_val_steps": 0,
+        'callbacks': [],
+        'logger': [],
+        'strategy': DeepSpeedStrategy(config=args.ds_config) \
+            if args.use_deepspeed else DDPStrategy(find_unused_parameters=False),
+        'num_nodes': args.num_nodes,
+        # 'plugins': plugins,
+        'precision': args.precision,
+        'accelerator': args.accelerator,
+        'devices': args.devices,
+        'max_epochs': args.max_epochs,
+        'max_steps': args.max_steps,
+        'val_check_interval': args.val_check_interval,
+        'log_every_n_steps': args.log_every_n_steps,
+        'accumulate_grad_batches': args.accumulate_grad_batches,
+        'gradient_clip_val': args.gradient_clip_val,
+        'gradient_clip_algorithm': 'norm',
+        'default_root_dir': args.default_root_dir,
+        # 'limit_val_batches': 0.0
+    }
+
+    trainer = pl.Trainer(**custom_trainer_kwargs)
+    logger.warning(f'{trainer.__dict__=}')
+
+    model.set_trainer_args(args)
+    logger.info('Initialized trainer')
+
+    trainer.validate(model, datamodule=data)
+    logger.info('Finished validation')
---
+++ test_java.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+#!/bin/bash
+
+DATA_PREFIX=ast_cfc
+NUM_STRUCTURE_TOKENS=10
+
+TRAIN_DATADIR=./stack2_java_test_1k
+VALID_DATADIR=./stack2_java_test_1k
+
+# TEXT_MODEL_ID=Qwen/Qwen2.5-Coder-1.5B
+TEXT_MODEL_ID=Qwen/Qwen2.5-Coder-7B
+# TEXT_MODEL_ID=Qwen/Qwen2.5-Coder-14B
+
+STRUCTURE_MODEL_ID=microsoft/unixcoder-base
+# STRUCTURE_MODEL_ID=Qwen/Qwen3-Embedding-0.6B
+
+MODEL_CHECKPOINT=./ckpt/qwen7_unixcoder_4l_java_2emes_stack2.ckpt
+
+TRAINING_STAGE=1
+NUM_DEVICES=2
+CUDA_LAUNCH_BLOCKING=1
+TORCH_USE_CUDA_DSA=1 
+python test_java.py \
+    --num_workers 96 \
+    --devices $NUM_DEVICES \
+    --num_nodes 1 \
+    --accelerator gpu \
+    --text_model_id $TEXT_MODEL_ID \
+    --structure_model_id $STRUCTURE_MODEL_ID \
+    --model_checkpoint $MODEL_CHECKPOINT \
+    --projector '4L' \
+    --dropout_p 0. \
+    --default_root_dir ./ \
+    --data_prefix $DATA_PREFIX \
+    --train_datadir $TRAIN_DATADIR \
+    --valid_datadir $VALID_DATADIR \
+    --log_dir ./logs/ \
+    --seed 1234 \
+    --lr 1e-5 \
+    --lr_scheduler_type None \
+    --weight_decay 0. \
+    --gradient_clip_val 1.0 \
+    --max_steps -1 \
+    --max_epochs 1 \
+    --warmup_steps 100 \
+    --train_batch_size $NUM_DEVICES \
+    --valid_batch_size $NUM_DEVICES \
+    --accumulate_grad_batches 32 \
+    --training_stage $TRAINING_STAGE \
+    --loss mle \
+    --alpha_ce .5 \
+    --alpha_scst .5 \
+    --log_every_n_steps 10 \
+    --save_step_frequency 10000000 \
+    --val_check_interval 10 \
+    --num_structure_tokens ${NUM_STRUCTURE_TOKENS} \
+    --precision 'bf16-true' \
+    --alpha_kl .0 \
+    --kl_temperature 1.0 \
+    --reward em+es \
+    --alpha_align 0.0 
